2fb3b00326c9c1509decf911b531f7f910c276e7
Dhruv Bansal <bansaldhruv1973@gmail.com>
2021-08-24 01:21:13 +0530

    :stars:

---
+++ L2/Questions/yet_another_broken_keyboard.cpp
@@ -0,0 +1,87 @@
+// https://codeforces.com/problemset/problem/1272/C
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <functional>
+#include <numeric>
+using namespace std;
+#define int long long 
+#define vi vector<int>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define tester int t;cin>>t;while(t--)
+#define looper(a,b,c) for(int a=b;a<c;a++)
+#define ss second
+#define pqmax priority_queue<int>
+#define pqmin priority_queue<int,vi,greater<int> >
+#define mod 1000000007
+#define vs vector<string>
+#define mp map<int,int>
+#define ump unordered_map<int,int>
+#define space ' '
+#define stc stack<char>
+#define sti stack<int>
+#define all(arr) arr.begin(),arr.end()
+#define looprev(i,a,b) for(int i=a;i>=(b);i--)
+#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<space;std::cout<<std::endl;
+#define si set<int>
+int gCd(int a,int b){
+    if(b==0)
+        return a;
+    return gCd(b,a%b);
+}
+int fact(int n){
+    int res=1;
+    for(int i=2;i<=n;i++)
+        res = (res*i)%mod;
+    return res;
+}
+int nCr(int n,int r){
+    return fact(n)/ (fact(r)*fact(n-r));
+}
+    
+bool compare(pair<int,int> &one, pair<int,int> &two){
+    return one.second < two.second;
+}
+int32_t main(){
+    int n,k;cin>>n>>k;
+    string s;cin>>s;
+    vi arr(26);
+    while(k--){
+        char ch;cin>>ch;arr[ch - 'a']=1;
+    }
+    vi temp;
+    temp.pb(-1);
+    looper(i,0,n){
+        if(!arr[s[i]-'a']){
+            temp.pb(i);
+        }
+    }
+    int ans=0;
+    temp.pb(n);
+    for(int i=0;i+1<temp.size();i++){
+        int curr = temp[i+1]-temp[i]-1;
+        ans+= (curr*(curr+1))/2;
+    }
+    cout<<ans;
+    
+    return 0;
+}
---
+++ L3/Questions/sum_of_sub_matrix.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <functional>
+#include <numeric>
+using namespace std;
+#define int long long 
+#define vi vector<int>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define pii pair<int,int>
+#define pb push_back
+#define ff first
+#define tester int t;cin>>t;while(t--)
+#define looper(a,b,c) for(int a=b;a<c;a++)
+#define ss second
+#define pqmax priority_queue<int>
+#define pqmin priority_queue<int,vi,greater<int> >
+#define mod 1000000007
+#define vs vector<string>
+#define mp map<int,int>
+#define ump unordered_map<int,int>
+#define space ' '
+#define stc stack<char>
+#define sti stack<int>
+#define all(arr) arr.begin(),arr.end()
+#define looprev(i,a,b) for(int i=a;i>=(b);i--)
+#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<space;std::cout<<std::endl;
+#define si set<int>
+int gCd(int a,int b){
+    if(b==0)
+        return a;
+    return gCd(b,a%b);
+}
+int fact(int n){
+    int res=1;
+    for(int i=2;i<=n;i++)
+        res = (res*i)%mod;
+    return res;
+}
+int nCr(int n,int r){
+    return fact(n)/ (fact(r)*fact(n-r));
+}
+    
+bool compare(pair<int,int> &one, pair<int,int> &two){
+    return one.second < two.second;
+}
+int32_t main(){
+    int n,m;cin>>n>>m;
+    vector<vi> arr(n+1,vi(m+1));
+    vector<vi> dp(n+1,vi(m+1));
+    looper(i,1,n+1){
+        looper(j,1,m+1){
+            int x;cin>>x;
+            arr[i][j]=x;dp[i][j]= dp[i-1][j] + dp[i][j-1] + x - dp[i-1][j-1];
+        }
+    }   
+    int q;cin>>q;
+    while(q--){
+        int x1,y1,x2,y2;cin>>x1>>y1>>x2>>y2;
+        int result= dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
+        cout<<result<<endl;
+    }
+    return 0;
+}
